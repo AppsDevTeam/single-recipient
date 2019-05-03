cffbf3db8a6c2d08a82ddadb8a2fd8d05f715b8b
Samuel Lisý <g.vvoody@gmail.com>
2019-05-03 09:32:47 +0200

    nette 3 compatibility

--- src/SingleRecipientMailer.php
+++ src/SingleRecipientMailer.php
@@ -26,7 +26,7 @@ class SingleRecipientMailer implements Mail\IMailer {
 		return !!$this->singleRecipient;
 	}
 
-	public function send(Mail\Message $mail) {
+	public function send(Mail\Message $mail): void {
 
 		if ($this->hasSingleRecipient()) {
 			$mail = clone $mail;
